d823cb4796691bf123a268b224e675137ed6d160
Kyle Lovell <ky2vell@gmail.com>
2020-04-19 21:33:37 -0400

    Stretch: added carousel component

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,60 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+// Image array for carousel
+
+const imgSrc = ['./assets/carousel/mountains.jpeg', './assets/carousel/computer.jpeg', './assets/carousel/trees.jpeg', './assets/carousel/turntable.jpeg'];
+
+// Create carousel component
+
+function carouselCreate() {
+  const carousel = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const rightButton = document.createElement('div');
+
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  leftButton.textContent = ' < ';
+  leftButton.setAttribute('onclick', 'plusSlides(-1)')
+  rightButton.textContent = ' > ';
+  rightButton.setAttribute('onclick', 'plusSlides(1)')
+
+  carousel.appendChild(leftButton);
+  carousel.appendChild(rightButton);
+
+  imgSrc.forEach(el => {
+    const images = document.createElement("img");
+    images.classList.add('slides');
+    images.src = el;
+    carousel.appendChild(images);
+  });
+
+  return carousel;
+}
+
+document.querySelector('.carousel-container').appendChild(carouselCreate());
+
+// Image carousel slideshow https://www.w3schools.com/howto/howto_js_slideshow.asp
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.querySelectorAll('.slides');
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
+}
+
+document.querySelector('.top-bar').style.zIndex = '9999';
